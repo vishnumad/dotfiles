eb2de7d609cbb29110659bdcfd2cad3e91b49af6
Vishnu <vmadhusoodanan95@gmail.com>
2019-04-19 17:33:10 -0500

    Change fonts to Input

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-sleep 4;
+#sleep 4;
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar top &
